070745ca87856ad130a46a55bc74454fb9a41c10
Award Malisi <awardmalisi@gmail.com>
2024-10-02 22:59:55 -0500

    footer building

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,19 @@
 <script>
 	import '@picocss/pico';
+	import Footer from '$lib/Footer.svelte';
 </script>
 
+<div>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
+<Footer />
+
+</div>
+
 <style lang="scss">
 	@import '$lib/styles/global.scss';
+
 
 	h1 {
 		color: $primary-red;
