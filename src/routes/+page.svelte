048dbe0d17847a251799f8336938437b0840b4ac
Award Malisi <awardmalisi@gmail.com>
2024-10-02 19:08:56 -0500

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import '@picocss/pico';
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style lang="scss">
+	@import '$lib/styles/global.scss';
+
+	h1 {
+		color: $primary-red;
+
+	}
+</style>
